39107bb1d843583dacdc5ced33f3da9a56251430
Antonio Dorado <antoniodavidorado@gmail.com>
2020-01-31 23:44:19 +0100

    some changes item styles

--- src/app/components/result-list/result-list.component.ts
+++ src/app/components/result-list/result-list.component.ts
@@ -11,62 +11,62 @@ export class ResultListComponent {
 
   products: any = [
     {
-      "name": "TV",
+      "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "TV",
+      "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "TV",
+      "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "TV",
+      "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "Switch",
+      "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
-      "name": "TV",
+      "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     }
