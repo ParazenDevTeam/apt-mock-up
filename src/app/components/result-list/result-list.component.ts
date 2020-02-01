71547a4610482f6bacc3695f7b9c1694ee6e0715
Antonio Dorado <antoniodavidorado@gmail.com>
2020-02-01 21:07:55 +0100

    item card v1

--- src/app/components/result-list/result-list.component.ts
+++ src/app/components/result-list/result-list.component.ts
@@ -13,61 +13,85 @@ export class ResultListComponent {
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
+      "category": "ELECTORNICA > TELEVISORES",
+      "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
+      "category": "ELECTORNICA > TELEVISORES",
+      "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
+      "category": "ELECTORNICA > TELEVISORES",
+      "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
+      "category": "ELECTORNICA > TELEVISORES",
+      "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
+      "category": "ELECTORNICA > CONSOLAS",
+      "bestprice": true,
       "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
       "price": 143.99,
+      "category": "ELECTORNICA > TELEVISORES",
+      "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
     }
   ];
