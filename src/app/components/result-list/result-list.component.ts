57ecb54cf53e873c0227a627e85d146514b11cf0
siwdie <codyjmc@gmail.com>
2020-02-03 21:26:59 +0100

    Reestructured cards and added shop images

--- src/app/components/result-list/result-list.component.ts
+++ src/app/components/result-list/result-list.component.ts
@@ -16,13 +16,15 @@ export class ResultListComponent {
       "category": "ELECTORNICA > TELEVISORES",
       "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
+      "shop": "amazon"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "amazon"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
@@ -30,13 +32,15 @@ export class ResultListComponent {
       "category": "ELECTORNICA > TELEVISORES",
       "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
+      "shop": "ebay"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "amazon"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
@@ -44,27 +48,31 @@ export class ResultListComponent {
       "category": "ELECTORNICA > TELEVISORES",
       "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
+      "shop": "ebay"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "ebay"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "amazon"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "amazon"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
@@ -72,20 +80,23 @@ export class ResultListComponent {
       "category": "ELECTORNICA > TELEVISORES",
       "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
+      "shop": "ebay"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "amazon"
     },
     {
       "name": "Nintendo Switch - Consola color Azul Neón/Rojo Neón (Modelo 2019) (B07WKNQ8JT)",
       "price": 349.00,
       "category": "ELECTORNICA > CONSOLAS",
       "bestprice": true,
-      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg"
+      "image": "https://thumb.pccomponentes.com/w-530-530/articles/23/233482/1.jpg",
+      "shop": "aliexpress"
     },
     {
       "name": "TV Televisión Televisor Engel Ever-LED LE2050 20 - HD - USB PVR- OCA-Modo Hotel (B07QVHD5BS)",
@@ -93,10 +104,15 @@ export class ResultListComponent {
       "category": "ELECTORNICA > TELEVISORES",
       "bestprice": false,
       "image": "https://images.samsung.com/is/image/samsung/latin-qled-q60r-row-qn75q60rapxpa-frontblack-163943271?$PD_GALLERY_L_JPG$",
+      "shop": "aliexpress"
     }
   ];
 
   constructor() {
+  }
+
+  getPhoto(shop: string) {
+    return '../../../assets/img/'+shop.toLowerCase()+'-icon.png';
   }
 
 }
