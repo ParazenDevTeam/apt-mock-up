6d14ca0b0dbb6b969c1892ffb685de9b9ac8b7cf
Antonio D. Dorado <antoniodavidorado@gmail.com>
2020-02-03 20:35:31 +0100

    Create category draw logic.

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PreferenceCategory {
+  header: string;
+  preferences: Preference[];
+}
+
+export interface Preference {
+  title: string;
+  onClick: Function;
+  icon: string;
+}
+
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
   styleUrls: ['./settings-page.component.scss']
 })
 export class SettingsPageComponent implements OnInit {
+
+  public preferences: PreferenceCategory[] = [
+    {
+      header: 'Catalog',
+      preferences: [
+        {title: 'Select shops', onClick: function() {
+          
+        }, icon: 'open_in_new'}
+      ]
+    }
+  ]
 
   constructor() { }
 
